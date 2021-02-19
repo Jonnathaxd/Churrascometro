f70b27e76809e9321263532382af78d68a891136
Geova Jonnatha <geova_jonnatha@hotmail.com>
2021-02-18 23:40:24 -0300

    update

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,13 @@
 // Refrigerante/agua - 1000 ml por pessoa + 6 horas 1500ml 
 
 //crianças vale por 0.5
+
+function active_button(button){
+    if(!button.dataset.active){
+        button.dataset.active = true
+
+    }else {
+        button.dataSet.active = false
+    }
+    console.log(button)
+}
